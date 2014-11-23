ebfd7c7126e3b274a43d9b82b7ae57b93ebda6cc
Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
2014-11-23 15:46:40 +0000

    some more examples
    merged asciidoc
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/mpe-java/trunk/org/tensorics/tensorics-core@8236 6cd15df7-5b2d-4548-a7df-5dcce267a22b

---
+++ src/examples/org/tensorics/core/examples/WorkingWithTensors.java
@@ -0,0 +1,13 @@
+package org.tensorics.core.examples;
+
+import org.tensorics.core.fields.doubles.Structures;
+import org.tensorics.core.lang.TensoricSupport;
+import org.tensorics.core.lang.Tensorics;
+import org.tensorics.core.tensor.operations.TensorInternals;
+
+public class WorkingWithTensors {
+	{
+		TensoricSupport<Double> support = Tensorics.using(Structures.doubles());
+
+	}
+}
