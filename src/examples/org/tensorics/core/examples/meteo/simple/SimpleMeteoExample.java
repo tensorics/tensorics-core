2f379b52884a64de9f1dbc01cb8788f50182933f
Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
2014-12-02 22:46:45 +0000

    some progress on the quickstart guide
    git-svn-id: https://svn.cern.ch/reps/mpe-java/trunk/org/tensorics/tensorics-core@8383 6cd15df7-5b2d-4548-a7df-5dcce267a22b

--- src/examples/org/tensorics/core/examples/meteo/simple/SimpleMeteoExample.java
+++ src/examples/org/tensorics/core/examples/meteo/simple/SimpleMeteoExample.java
@@ -1,27 +1,40 @@
 package org.tensorics.core.examples.meteo.simple;
 
 import org.tensorics.core.lang.Tensorics;
+import org.tensorics.core.tensor.Position;
 import org.tensorics.core.tensor.Tensor;
+import org.tensorics.core.tensor.Tensor.Entry;
 import org.tensorics.core.tensor.TensorBuilder;
 
 public class SimpleMeteoExample {
 
 	@SuppressWarnings("unused")
 	public void example() {
-// tag::createTensor[]
-TensorBuilder<Double> builder = Tensorics.builder(City.class, Day.class); // <1>
+		// tag::createTensor[]
+		TensorBuilder<Double> builder = Tensorics.builder(City.class, Day.class); // <1>
 
-builder.putAt(18.5, City.NEW_YORK, Day.APRIL_1_2014); // <2>
-builder.putAt(25.0, City.NEW_YORK, Day.JUNE_1_2014);
-builder.putAt(19.8, City.GENEVA, Day.APRIL_1_2014);
-builder.putAt(24.7, City.GENEVA, Day.JUNE_1_2014);
+		builder.putAt(18.5, City.NEW_YORK, Day.APRIL_1_2014); // <2>
+		builder.putAt(25.0, City.NEW_YORK, Day.JUNE_1_2014);
+		builder.putAt(19.8, City.GENEVA, Day.APRIL_1_2014);
+		builder.putAt(24.7, City.GENEVA, Day.JUNE_1_2014);
 
-Tensor<Double> temperatures = builder.build(); // <3>
-// end::createTensor[]
+		Tensor<Double> temperatures = builder.build(); // <3>
+		// end::createTensor[]
 
-//tag::getValue[]
-double temperature = temperatures.get(City.NEW_YORK, Day.APRIL_1_2014);
-//end::getValue[]
+		// tag::getValue[]
+		double temperature = temperatures.get(City.NEW_YORK, Day.APRIL_1_2014);
+		// end::getValue[]
 
+		// tag::loopThroughEntries[]
+		for (Entry<Double> entry : temperatures.entrySet()) {
+			Position position = entry.getPosition();
+			double value = entry.getValue();
+			System.out.println(position + "\t->\t" + value);
+		}
+		// end::loopThroughEntries[]
+	}
+
+	public static void main(String[] args) {
+		new SimpleMeteoExample().example();
 	}
 }
