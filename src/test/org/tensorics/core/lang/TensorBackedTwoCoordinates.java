ca8f6d5b993e5d277fddfeab2c5977a9aad692f8
Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
2014-12-05 15:23:09 +0000

    new entries method in tensorbacked
    git-svn-id: https://svn.cern.ch/reps/mpe-java/trunk/org/tensorics/tensorics-core@8441 6cd15df7-5b2d-4548-a7df-5dcce267a22b

--- src/test/org/tensorics/core/lang/TensorBackedTwoCoordinates.java
+++ src/test/org/tensorics/core/lang/TensorBackedTwoCoordinates.java
@@ -5,22 +5,15 @@
 package org.tensorics.core.lang;
 
 import org.tensorics.core.tensor.Tensor;
+import org.tensorics.core.tensorbacked.AbstractTensorbacked;
 import org.tensorics.core.tensorbacked.Tensorbacked;
 import org.tensorics.core.tensorbacked.annotation.Dimensions;
 
 @Dimensions({ XCoordinate.class, YCoordinate.class })
-public class TensorBackedTwoCoordinates implements Tensorbacked<Double> {
-
-    private final Tensor<Double> tensor;
+public class TensorBackedTwoCoordinates extends AbstractTensorbacked<Double> {
 
     public TensorBackedTwoCoordinates(Tensor<Double> tensor) {
-        super();
-        this.tensor = tensor;
-    }
-
-    @Override
-    public Tensor<Double> tensor() {
-        return tensor;
+        super(tensor);
     }
 
 }
