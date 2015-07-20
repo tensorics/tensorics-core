9faef704f1c206c569d56f62c1bb8f6e65dfddbd
Arkadiusz Gorzawski <Arkadiusz.Gorzawski@cern.ch>
2015-07-20 21:35:40 +0000

    finished the refactoring changes
    git-svn-id: svn+ssh://svn.cern.ch/reps/mpe-java/trunk/org/tensorics/tensorics-core@10000 6cd15df7-5b2d-4548-a7df-5dcce267a22b

--- src/java/org/tensorics/core/tensor/specific/ImmutableDoubleArrayBackedTensor.java
+++ src/java/org/tensorics/core/tensor/specific/ImmutableDoubleArrayBackedTensor.java
@@ -15,104 +15,104 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
 /**
- * A specific implementation of a tensor, that contains double values. It is backed by a simple double array to minimize
- * memory usage and improve performance.
+ * A specific implementation of a tensor, that contains double values. It is
+ * backed by a simple double array to minimize memory usage and improve
+ * performance.
  * 
  * @author kaifox
  */
 public class ImmutableDoubleArrayBackedTensor implements Tensor<Double> {
 
-    private final PositionIndexer indexer;
-    private final double[] values;
-    private final Context tensorContext;
+	private final PositionIndexer indexer;
+	private final double[] values;
+	private final Context tensorContext;
 
-    public ImmutableDoubleArrayBackedTensor(Builder builder) {
-        this.indexer = builder.indexer;
-        this.values = Arrays.copyOf(builder.values, builder.values.length);
-        this.tensorContext = builder.getContext();
-    }
+	public ImmutableDoubleArrayBackedTensor(Builder builder) {
+		this.indexer = builder.indexer;
+		this.values = Arrays.copyOf(builder.values, builder.values.length);
+		this.tensorContext = builder.getContext();
+	}
 
-    @Override
-    public Double get(Position position) {
-        return values[indexer.indexFor(position)];
-    }
+	@Override
+	public Double get(Position position) {
+		return values[indexer.indexFor(position)];
+	}
 
-    @Override
-    public Double get(Object... coordinates) {
-        return get(Position.of(coordinates));
-    }
+	@Override
+	public Double get(Object... coordinates) {
+		return get(Position.of(coordinates));
+	}
 
-    @Override
-    @Deprecated
-    public Set<Tensor.Entry<Double>> entrySet() {
-        ImmutableSet.Builder<Tensor.Entry<Double>> builder = ImmutableSet.builder();
-        for (Position position : indexer.allPositions()) {
-            builder.add(new ImmutableEntry<Double>(position, get(position)));
-        }
-        return null;
-    }
-
+	@Override
+	@Deprecated
+	public Set<Tensor.Entry<Double>> entrySet() {
+		ImmutableSet.Builder<Tensor.Entry<Double>> builder = ImmutableSet
+				.builder();
+		for (Position position : indexer.allPositions()) {
+			builder.add(new ImmutableEntry<Double>(position, get(position)));
+		}
+		return builder.build();
+	}
 
 	@Override
 	public Map<Position, Double> asMap() {
 		ImmutableMap.Builder<Position, Double> builder = ImmutableMap.builder();
-        for (Position position : indexer.allPositions()) {
-            builder.put(position, get(position));
-        }
+		for (Position position : indexer.allPositions()) {
+			builder.put(position, get(position));
+		}
 		return builder.build();
 	}
-    
-    @Override
-    public Shape shape() {
-        return Shape.viewOf(indexer.dimensions(), indexer.allPositions());
-    }
 
-    @Override
-    public Context context() {
-        return this.tensorContext;
-    }
+	@Override
+	public Shape shape() {
+		return Shape.viewOf(indexer.dimensions(), indexer.allPositions());
+	}
 
-    public static Builder builder(PositionIndexer indexer) {
-        return new Builder(indexer);
-    }
+	@Override
+	public Context context() {
+		return this.tensorContext;
+	}
 
-    /**
-     * A builder for tensor which only will contain doubles
-     * 
-     * @author kfuchsbe
-     */
-    public static class Builder extends AbstractTensorBuilder<Double> {
+	public static Builder builder(PositionIndexer indexer) {
+		return new Builder(indexer);
+	}
 
-        private final PositionIndexer indexer;
-        private final double[] values;
+	/**
+	 * A builder for tensor which only will contain doubles
+	 * 
+	 * @author kfuchsbe
+	 */
+	public static class Builder extends AbstractTensorBuilder<Double> {
 
-        Builder(PositionIndexer indexer) {
-            super(indexer.dimensions());
-            this.indexer = indexer;
-            this.values = new double[indexer.arraySize()];
-        }
+		private final PositionIndexer indexer;
+		private final double[] values;
 
-        @Override
-        protected void putItAt(Double value, Position position) {
-            this.values[indexer.indexFor(position)] = value;
-        }
+		Builder(PositionIndexer indexer) {
+			super(indexer.dimensions());
+			this.indexer = indexer;
+			this.values = new double[indexer.arraySize()];
+		}
 
-        public void putUncheckedAt(double value, Position position) {
-            this.values[indexer.indexFor(position)] = value;
-        }
+		@Override
+		protected void putItAt(Double value, Position position) {
+			this.values[indexer.indexFor(position)] = value;
+		}
 
-        @Override
-        public ImmutableDoubleArrayBackedTensor build() {
-            return new ImmutableDoubleArrayBackedTensor(this);
-        }
+		public void putUncheckedAt(double value, Position position) {
+			this.values[indexer.indexFor(position)] = value;
+		}
+
+		@Override
+		public ImmutableDoubleArrayBackedTensor build() {
+			return new ImmutableDoubleArrayBackedTensor(this);
+		}
 
 		@Override
 		public void putAll(Map<Position, Double> newEntries) {
-			// TODO Auto-generated method stub
-			
+			for (java.util.Map.Entry<Position, Double> one : newEntries
+					.entrySet()) {
+				putItAt(one.getValue(), one.getKey());
+			}
 		}
-
-    }
-
-
+	}
 }
