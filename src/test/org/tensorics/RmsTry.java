2b5c2ce5788cb552de946776cdefe204249a7b7a
Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
2015-09-11 23:04:53 +0000

    some new methods
    git-svn-id: svn+ssh://svn.cern.ch/reps/mpe-java/trunk/org/tensorics/tensorics-core@10660 6cd15df7-5b2d-4548-a7df-5dcce267a22b

---
+++ src/test/org/tensorics/RmsTry.java
@@ -0,0 +1,17 @@
+
+package org.tensorics;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RmsTry {
+
+    @Test
+    public void test() {
+        
+        
+        fail("Not yet implemented");
+    }
+
+}
