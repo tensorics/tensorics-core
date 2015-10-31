70df40320f88ae73bd9a7b97d939549fbec12dc0
KamilKrol <kamil.h.krol@gmail.com>
2015-10-31 07:14:29 +0100

    Commit to force Travis CI

---
+++ src/test/org/tensorics/KamilTest.java
@@ -0,0 +1,19 @@
+package org.tensorics;
+
+import org.junit.Test;
+
+/**
+ * Created by kkrol on 29/10/2015.
+ */
+public class KamilTest {
+
+    @Test
+    public void testDummy() {
+
+    }
+    
+    @Test
+    public void testDummy2() {
+        
+    }
+}
