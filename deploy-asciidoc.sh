71898551600b68ff43bfc698258f20a2dd5724ca
Kamil Krol <kamil.krol@cern.ch>
2015-11-21 15:49:07 +0100

    index.html should not be removed automatically!

--- deploy-asciidoc.sh
+++ deploy-asciidoc.sh
@@ -12,7 +12,6 @@ git config user.email "${E_MAIL}"
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add tensorics*
-git reset index.html
 git commit -m "Deploy to GitHub Pages"
 
 # Force push from the current repo's master branch to the remote
