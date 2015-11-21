218fb460fc424880e43947d2c4e5980de47c9029
Kamil Krol <kamil.krol@cern.ch>
2015-11-21 17:07:35 +0100

    Javadocs

--- deploy-asciidoc.sh
+++ deploy-asciidoc.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-# go to the out directory and create a *new* Git repo
-cd build/asciidoc
-git init
-
-# inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
 git config user.email "${E_MAIL}"
 
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
+cd build/asciidoc
+git init
+
 git add .
 git commit -m "Automatically generated GitHub Pages"
+
+cd ../docs
+git init
+
+git add .
+git commit -m "Automatically generated Javadocs Pages"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
