e0acb894ed70e007df76c017423051c0f8baaf1c
Kamil Krol <kamil.krol@cern.ch>
2015-11-21 16:23:57 +0100

    Changes in the deploy-asciidoc.sh

--- deploy-asciidoc.sh
+++ deploy-asciidoc.sh
@@ -12,11 +12,10 @@ git config user.email "${E_MAIL}"
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Automatically generated GitHub Pages"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-#git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages  > /dev/null 2>&1
