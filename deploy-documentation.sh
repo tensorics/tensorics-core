ee144c944cc9c9a4bf36c17974dace14a07e9ff4
KamilKrol <kamil.h.krol@gmail.com>
2015-11-21 18:08:49 +0100

    Proper order of git commands

--- deploy-documentation.sh
+++ deploy-documentation.sh
@@ -7,12 +7,13 @@ cp -r build/asciidoc/* build/documentation/
 cp -r build/docs/* build/documentation/
 
 cd build/documentation
+
 git init
+git config user.name "Travis CI"
+git config user.email "${E_MAIL}"
+
 git add .
 git commit -m "Automatically generated GitHub Pages and Javadocs"
-
-git --global config user.name "Travis CI"
-git --global config user.email "${E_MAIL}"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
