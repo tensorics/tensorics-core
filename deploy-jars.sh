4ecc7167fb62fcb0baf3d60a952bcdb090390597
Andrea Calia <andrea.calia@cern.ch>
2016-07-27 14:58:06 +0200

    test travis

--- deploy-jars.sh
+++ deploy-jars.sh
@@ -9,5 +9,8 @@ secreteFilePath=$(pwd)/SecretFile.pgp
 echo "############ secreteFilePath =" $secreteFilePath
 
 #gradle uploadArchives -Psigning.keyId=${signing.keyId} -Psigning.password=${signing.password}  -Psigning.secretKeyRingFile=$secreteFilePath -PossrhUsername=${ossrhUsername} -PossrhPassword=${ossrhPassword}
-
-echo "-Psigning.keyId=${signing.keyId} -Psigning.password=${signing.password} -Psigning.secretKeyRingFile=$secreteFilePath -PossrhUsername=${ossrhUsername} -PossrhPassword=${ossrhPassword}"
+echo "${signing.keyId}"
+echo "${signing.password}"
+echo "${ossrhUsername}"
+echo "${ossrhPassword}"
+#echo "-Psigning.keyId=${signing.keyId} -Psigning.password=${signing.password} -Psigning.secretKeyRingFile=$secreteFilePath -PossrhUsername=${ossrhUsername} -PossrhPassword=${ossrhPassword}"
