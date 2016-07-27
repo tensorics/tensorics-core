52fad44d577a83c772a5aa3b522b5b1d4feceae3
Andrea Calia <andrea.calia@cern.ch>
2016-07-27 18:49:04 +0200

    gpg does not ask anything

--- deploy-jars.sh
+++ deploy-jars.sh
@@ -3,11 +3,14 @@ set -e # exit with nonzero exit code if anything fails
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     
+	echo "Decrypting codesigning.asc.enc..."
 	openssl aes-256-cbc -K $encrypted_e60aa78a0bb7_key -iv $encrypted_e60aa78a0bb7_iv -in codesigning.asc.enc -out codesigning.asc -d
-    gpg --fast-import codesigning.asc
-
+    echo "... done"
+    gpg --trust-model always --fast-import codesigning.asc
+    echo "Key added"
 	secreteFilePath=$(pwd)/codesigning.asc
-	
+	echo "Secrete key path found"
+	echo "Executing gradle uploadArchives"
 	gradle uploadArchives -Psigning.keyId=${signingKeyId} -Psigning.password=${signingPassword}  -Psigning.secretKeyRingFile=$secreteFilePath -PossrhUsername=${ossrhUsername} -PossrhPassword=${ossrhPassword}
 	
 	rm $secreteFilePath
