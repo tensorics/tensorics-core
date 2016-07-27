5b49f1c583adf43e0efb3301be388710bf201aca
Andrea Calia <andrea.calia.46@gmail.com>
2016-07-27 09:07:44 -0700

    travis encrypt from linux

--- deploy-jars.sh
+++ deploy-jars.sh
@@ -3,7 +3,7 @@ set -e # exit with nonzero exit code if anything fails
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     
-	openssl aes-256-cbc -K $encrypted_7fc09654fb2e_key -iv $encrypted_7fc09654fb2e_iv -in codesigning.asc.enc -out codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_e60aa78a0bb7_key -iv $encrypted_e60aa78a0bb7_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import codesigning.asc
 
 	secreteFilePath=$(pwd)/codesigning.asc
