119f6ca3834c61bf0df26714d4e4cae6a34c6a60
Andrea Calia <andrea.calia@cern.ch>
2016-07-27 19:25:22 +0200

    try to use gpg2

--- deploy-jars.sh
+++ deploy-jars.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
 	echo "Decrypting codesigning.asc.enc..."
 	openssl aes-256-cbc -K $encrypted_e60aa78a0bb7_key -iv $encrypted_e60aa78a0bb7_iv -in codesigning.asc.enc -out codesigning.asc -d
     echo "... done"
-    gpg --batch --personal-cipher-preferences "AES256 AES192 AES CAST5 3DES" --personal-digest-preferences "SHA256 SHA1 SHA384 SHA512 SHA224" --personal-compress-preferences "ZLIB BZIP2 ZIP Uncompressed" --fast-import codesigning.asc
+    gpg2 --batch --personal-cipher-preferences "AES256 AES192 AES CAST5 3DES" --personal-digest-preferences "SHA256 SHA1 SHA384 SHA512 SHA224" --personal-compress-preferences "ZLIB BZIP2 ZIP Uncompressed" --fast-import codesigning.asc
     echo "Key added"
 	secreteFilePath=$(pwd)/codesigning.asc
 	echo "Secrete key path found"
