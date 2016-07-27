8ce4d89cf93efd13e0a7a52db779adf07f4f21ae
Andrea Calia <andrea.calia@cern.ch>
2016-07-27 14:41:46 +0200

    test for travis deploy

---
+++ deploy-jars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+#OPENSSH.......
+touch SecretFile.pgp
+
+secreteFilePath=$(pwd)/SecretFile.pgp
+
+echo "############ secreteFilePath =" $secreteFilePath
+
+#gradle uploadArchives -Psigning.keyId=${signing.keyId} -Psigning.password=${signing.password}  -Psigning.secretKeyRingFile=$secreteFilePath -PossrhUsername=${ossrhUsername} -PossrhPassword=${ossrhPassword}
+
+echo "-Psigning.keyId=${signing.keyId} -Psigning.password=${signing.password} -Psigning.secretKeyRingFile=$secreteFilePath -PossrhUsername=${ossrhUsername} -PossrhPassword=${ossrhPassword}"
