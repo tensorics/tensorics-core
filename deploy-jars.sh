ba632af6c42e2c26bbff891f76ea955645dd7322
Andrea Calia <andrea.calia.46@gmail.com>
2016-07-28 02:06:14 -0700

    fix gradle uploadJars

--- deploy-jars.sh
+++ deploy-jars.sh
@@ -3,12 +3,10 @@ set -e # exit with nonzero exit code if anything fails
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     
-	echo "Decrypting codesigning.asc.enc..."
-	openssl aes-256-cbc -K $encrypted_e60aa78a0bb7_key -iv $encrypted_e60aa78a0bb7_iv -in codesigning.asc.enc -out codesigning.asc -d
+	echo "Decrypting deployment.key.enc..."
+	openssl aes-256-cbc -K $encrypted_e60aa78a0bb7_key -iv $encrypted_e60aa78a0bb7_iv -in deployment.key.enc -out deployment.key -d
     echo "... done"
-    #gpg2 --batch --personal-cipher-preferences "AES256 AES192 AES CAST5 3DES" --personal-digest-preferences "SHA256 SHA1 SHA384 SHA512 SHA224" --personal-compress-preferences "ZLIB BZIP2 ZIP Uncompressed" --fast-import codesigning.asc
-    #echo "Key added"
-	secreteFilePath=$(pwd)/codesigning.asc
+	secreteFilePath=$(pwd)/deployment.key
 	echo "Secrete key path found"
 	echo "Executing gradle uploadArchives"
 	gradle uploadArchives -Psigning.keyId=${signingKeyId} -Psigning.password=${signingPassword}  -Psigning.secretKeyRingFile=$secreteFilePath -PossrhUsername=${ossrhUsername} -PossrhPassword=${ossrhPassword}
