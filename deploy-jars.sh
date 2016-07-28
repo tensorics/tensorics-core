06f971059363c083280265a20b18cff291c1885c
Andrea Calia <andrea.calia@cern.ch>
2016-07-28 10:49:10 +0200

    try to use only gradle to sign

--- deploy-jars.sh
+++ deploy-jars.sh
@@ -6,8 +6,8 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
 	echo "Decrypting codesigning.asc.enc..."
 	openssl aes-256-cbc -K $encrypted_e60aa78a0bb7_key -iv $encrypted_e60aa78a0bb7_iv -in codesigning.asc.enc -out codesigning.asc -d
     echo "... done"
-    gpg2 --batch --personal-cipher-preferences "AES256 AES192 AES CAST5 3DES" --personal-digest-preferences "SHA256 SHA1 SHA384 SHA512 SHA224" --personal-compress-preferences "ZLIB BZIP2 ZIP Uncompressed" --fast-import codesigning.asc
-    echo "Key added"
+    #gpg2 --batch --personal-cipher-preferences "AES256 AES192 AES CAST5 3DES" --personal-digest-preferences "SHA256 SHA1 SHA384 SHA512 SHA224" --personal-compress-preferences "ZLIB BZIP2 ZIP Uncompressed" --fast-import codesigning.asc
+    #echo "Key added"
 	secreteFilePath=$(pwd)/codesigning.asc
 	echo "Secrete key path found"
 	echo "Executing gradle uploadArchives"
