1cf83112c3421b75e6106852a4bfb5fd38c58a1d
Andrea Calia <andrea.calia@cern.ch>
2016-08-01 14:30:31 +0200

    try to use tags in travis

--- deploy-jars.sh
+++ deploy-jars.sh
@@ -3,13 +3,16 @@ set -e # exit with nonzero exit code if anything fails
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     
-	echo "Decrypting deployment.key.enc..."
 	openssl aes-256-cbc -K $encrypted_e60aa78a0bb7_key -iv $encrypted_e60aa78a0bb7_iv -in deployment.key.enc -out deployment.key -d
-    echo "... done"
-	secreteFilePath=$(pwd)/deployment.key
-	echo "Secrete key path found"
+    secreteFilePath=$(pwd)/deployment.key
+	echo "Deployment key found"
 
 	echo "DEBUG"
+	git status
+	git tag
+	ls -l
+	echo "Tag: $TRAVIS_TAG"
+	echo "Commit: $TRAVIS_COMMIT"
 	#gpg --recv-keys 4170AA62
 	#./gradlew -v
 	#./gradlew signArchives -Psigning.keyId=${signingKeyId} -Psigning.password=${signingPassword}  -Psigning.secretKeyRingFile=$secreteFilePath -PossrhUsername=${ossrhUsername} -PossrhPassword=${ossrhPassword} --stacktrace --info
