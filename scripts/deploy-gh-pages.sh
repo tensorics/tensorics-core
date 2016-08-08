ddb83a05002d58cfc8d1cdffc3c7774adaac75e2
Andrea Calia <andrea.calia@cern.ch>
2016-08-08 17:10:44 +0200

    updated automatic gh pages deployment and asciidoc files

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-# go to the out directory and create a *new* Git repo
+gradle javadoc asciidoctor
+
+echo "Gathering files"
 cd build/asciidoc
+# copy the documentation
+cp ../docs/javadoc . -r
+
+echo "Init new git repo"
 git init
+git config user.name "tensorics-dev"
+git config user.email "tensorics-dev@cern.ch"
+git add . &> /dev/null
+git commit -m "Automatic deployment to GitHub Pages" &> /dev/null
 
-# inside this git repo we'll pretend to be a new user
-git config user.name "Travis CI"
-git config user.email "kaifox@gmx.at"
-
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
-git add .
-git commit -m "Deploy to GitHub Pages"
-
+echo "Pushing to gh-pages"
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
