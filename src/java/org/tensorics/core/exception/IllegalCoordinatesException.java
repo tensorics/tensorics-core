1f1a148244cf092f7e22b0791f4fc57df37e8be9
Arek Gorzawski <agorzawski@gmail.com>
2016-09-20 20:01:45 +0200

    missing class

---
+++ src/java/org/tensorics/core/exception/IllegalCoordinatesException.java
@@ -0,0 +1,14 @@
+package org.tensorics.core.exception;
+
+/**
+ * An exception to be thrown when the inconsistency of the coordinates is
+ * detected ie. coordinate class implements two interfaces that are separately
+ * defined as an dimension.
+ * 
+ * @author arek
+ */
+public class IllegalCoordinatesException extends IllegalArgumentException {
+
+	private static final long serialVersionUID = 1L;
+
+}
