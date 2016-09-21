c87a5fcfc5dd52fd1aae2168d28000be61cf1a67
Kajetan Fuchsberger <kajetan.fuchsberger@cern.ch>
2016-09-21 17:58:11 +0200

    Hashcode and Equals for resolved expression

--- src/java/org/tensorics/core/tree/domain/ResolvedExpression.java
+++ src/java/org/tensorics/core/tree/domain/ResolvedExpression.java
@@ -72,4 +72,34 @@ public final class ResolvedExpression<R> implements Expression<R>, Named {
         return Objects.toString(value);
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ResolvedExpression<?> other = (ResolvedExpression<?>) obj;
+        if (value == null) {
+            if (other.value != null) {
+                return false;
+            }
+        } else if (!value.equals(other.value)) {
+            return false;
+        }
+        return true;
+    }
+
 }
